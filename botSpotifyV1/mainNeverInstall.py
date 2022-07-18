fb05fae49590aca56547a2c50364fbfd104753af
lips1982 <42499047+lips1982@users.noreply.github.com>
2022-07-17 20:21:26 -0500

    Update mainNeverInstall.py

--- botSpotifyV1/mainNeverInstall.py
+++ botSpotifyV1/mainNeverInstall.py
@@ -107,6 +107,7 @@ def iniciarEscritorio():
                         break
                     if time.time()-time_start>=420:
                         pyautogui.screenshot(os.path.join(pathImg,f"{cuenta}-7minutosscreen-{str(datetime.datetime.now().strftime('%H-%M-%S'))}.png"))
+                        break
                 pyautogui.screenshot(os.path.join(pathImg,f"04.1-{cuenta}.png"))
 
                 iniciarEscritorio()
