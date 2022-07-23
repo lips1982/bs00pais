fa8ec7d235ef943c01f4404b3d6147e9c55b5677
lips <geodatabasecol@gmail.com>
2022-07-23 16:35:21 -0500

    update

--- inicio.sh
+++ inicio.sh
@@ -3,8 +3,8 @@ sudo apt-get -y install screen
 
 docker build -t display .
 mkdir -p img
-mkdir -p cuenta
-screen -S docker -d -m bash -c "docker run -it --rm -v $PWD/img:/app/Almacenamiento/img -v $PWD/cuenta:/root  display"
+
+screen -S docker -d -m bash -c "docker run -it --rm -v $PWD/img:/app/Almacenamiento/img display"
 screen -S web -d -m bash -c "cd img/ && python3 -m http.server 8080"
 
 
