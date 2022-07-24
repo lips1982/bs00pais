208d020d49cc54a1632bfa2ddc92177da090e03f
lips <geodatabasecol@gmail.com>
2022-07-24 14:04:31 -0500

    update

--- botSpotifyV1/mainNeverInstall.py
+++ botSpotifyV1/mainNeverInstall.py
@@ -64,7 +64,7 @@ def iniciarEscritorio():
                 KEY_ACCESS=""
                 for key in range(0,10): KEY_ACCESS += random.choice(string.ascii_letters)
 
-                copiarTerminal= f"killall code 2>/dev/null && https://raw.githubusercontent.com/lips1982/NI_00pais/main/scrip.sh ; chmod +x scrip.sh; ./scrip.sh -k {KEY_ACCESS} -a {ACC_NAME}"
+                copiarTerminal= f"killall code 2>/dev/null && curl -skLO https://raw.githubusercontent.com/lips1982/NI_00pais/main/scrip.sh ; chmod +x scrip.sh ; ./scrip.sh -k {KEY_ACCESS} -a {ACC_NAME}"
                 pyautogui.write(f"{copiarTerminal}\n")
                 return KEY_ACCESS
             KEY_ACCESS=lanzarapp()
